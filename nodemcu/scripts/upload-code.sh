5a1bf63bb8eb71e4d7cffe765e871d112e43210a
titosilva <jt.mat@hotmail.com>
2022-05-03 00:17:18 -0300

    feat: added initial code

--- nodemcu/scripts/upload-code.sh
+++ nodemcu/scripts/upload-code.sh
@@ -4,8 +4,13 @@ folder="../src"
 
 echo "Uploading codes from $folder"
 
-for f in $(ls $folder)
-do
-    echo "Uploading file $f..."
-    nodemcu-tool upload "$folder/$f"
-done
+if [ -z $1 ]
+then
+    for f in $(ls $folder)
+    do
+        echo "Uploading file $f..."
+        nodemcu-tool upload "$folder/$f"
+    done
+else
+    nodemcu-tool upload "$folder/$1"
+fi
